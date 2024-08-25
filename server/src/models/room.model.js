afa1bb222d050f25a83bea982f1e49b0c8c114bc
Anurag <anuragpoddar9484@gmail.com>
2024-08-25 20:14:13 +0530

    add: user and room model, configure jwt token

---
+++ server/src/models/room.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    participants: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+        role: {
+          type: String,
+          enum: ['host', 'participant'],
+          default: 'participant',
+        },
+        joinedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      }
+    ],
+    isActive: {
+      type: Boolean,
+      default: true,
+    }
+  }, { timestamps: true })
+
+export const Room = mongoose.model("Room", roomSchema)
---
+++ server/src/models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: [true, "Username is required"],
+      lowercase: true,
+      unique: [true, "Username is already takken"]
+    },
+    email: {
+      type: String,
+      require: [true, "Email is Required"],
+      unique: [true, "Email already takken"],
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      require: [true, "Password is required"],
+    },
+    profilePic: {
+      type: String, // optional from clounary aur any other service
+    },
+    refreshToken: {
+      type: String,
+    }
+  }
+  , {timestamps: true}
+)
+
+userSchema.pre("save", async function(next){
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      password: this.password,
+    },
+    process.env.ACCESS_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema);
