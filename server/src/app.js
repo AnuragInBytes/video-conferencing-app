79674ea7e491c0398a70475c993c2e382c00b994
Anurag <anuragpoddar9484@gmail.com>
2024-09-06 13:57:02 +0530

    tried adding socket io in server

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import { ApiError } from './utils/ApiError.js';
 
 const app = express()
 
@@ -10,21 +9,6 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
 }))
-
-// app.use((err, req, res, next) => {
-//   if(err instanceof ApiError) {
-//     return res.status(err.statusCode).json({
-//       status: err.statusCode,
-//       message: err.message,
-//       success: err.success,
-//       error: err.errors.length > 0 ? err.errors : undefined,
-//     });
-//   }
-//   return res.status(500).json({
-//     status: 500,
-//     message: "Internal server error",
-//   });
-// });
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
@@ -38,7 +22,6 @@ import userRouter from './routes/user.routes.js'
 app.use("/api/v1/users", userRouter)
 
 // http://localhost800/api/v1/users/<user.routers.js>
-
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -50,4 +33,3 @@ app.use((err, req, res, next) => {
 });
 
 export { app }
-
