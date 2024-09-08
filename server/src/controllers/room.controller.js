00a52c04141c49f8497170b14f654aca407a6b81
Anurag <anuragpoddar9484@gmail.com>
2024-09-08 11:50:50 +0530

    added room controller

---
+++ server/src/controllers/room.controller.js
@@ -0,0 +1,66 @@
+import { Room } from '../models/room.model'
+import { ApiError } from '../utils/ApiError';
+import { ApiResponse } from '../utils/ApiResponse';
+import { asyncHandler } from '../utils/asyncHandler'
+
+const registerRoom = asyncHandler( async (req, res) => {
+
+  const { title, startTime } = req.body;
+  const createdBy = req.user._id; //TODO: need a fix as required in future
+
+  // if(!title?.trim() || !startTime){
+  //   throw new ApiError(400, "All fields are required");
+  // }
+  if(
+    [title, startTime, createdBy].some((field) => field?.trim() === "")
+  ){
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const start = new Date(startTime);
+  if(isNaN(start.getTime())){
+    throw new ApiError(400, "Invalid start time provided")
+  }
+
+  const room = await Room.create({
+    title,
+    createdBy,
+    startTime,
+  });
+
+  const createdRoom = await Room.findById(room._id)
+
+  if(!createdRoom) throw new ApiError(500, "Something went wrong while creating room");
+
+  return res.status(201).json(
+    new ApiResponse(200, createdRoom, "Room successfully created")
+  );
+
+})
+
+const joinRoom = asyncHandler( async(req, res) => {
+  const { roomId } = req.params;
+  const userId = req.user._id;
+
+  const room = await Room.findById(roomId)
+
+  //check if the room exist or not
+  if(!room){
+    throw new ApiError(400, "Room not found");
+  }
+
+  //check the participant is already in some room or not
+  const alreadyIn = (participant) => participant.userId.toString() === userId.String();
+  const isPartOf = room.participants.some(alreadyIn);
+
+  if(!isPartOf){
+    room.participants.push({ userId });
+    await room.save();
+  }
+
+  return res.status(200).json(
+    new ApiResponse(200, room, "Room Joined successfully")
+  );
+});
+
+export { registerRoom, joinRoom }
