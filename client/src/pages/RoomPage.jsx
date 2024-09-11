05c2a791cca57f8bc892e0c0ed766a1024e742aa
Anurag <anuragpoddar9484@gmail.com>
2024-09-12 02:01:02 +0530

    fixes pages

---
+++ client/src/pages/RoomPage.jsx
@@ -0,0 +1,12 @@
+import { Room } from '@/components'
+import React from 'react'
+
+function RoomPage() {
+  return (
+    <div className='w-full h-screen bg-[#1c1f2e] px-5 py-3 '>
+      <Room />
+    </div>
+  )
+}
+
+export default RoomPage
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,11 @@
 import HomePage from "./HomePage";
 import SignInPage from "./SignInPage";
 import SignUpPage from "./SignUpPage";
+import RoomPage from "./RoomPage";
 
 export {
   HomePage,
   SignInPage,
   SignUpPage,
+  RoomPage,
 }
